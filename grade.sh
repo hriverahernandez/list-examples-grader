e31a2b35dc61c09797579eb61124a70d10b17ef7
hriverahernandez <heriberto.rivera0325@gmail.com>
2023-05-11 15:47:37 -0700

    Added code

--- grade.sh
+++ grade.sh
@@ -14,3 +14,27 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+if [[ -f student-submission/ListExamples.java ]]
+then
+  echo 'ListExamples.java found!'
+else
+  echo 'ListExamples.java not found'
+  echo 'Score 0/4'
+  exit 1
+fi 
+
+cp TestListExamples.java ./grading-area
+cp student-submission/ListExamples.java ./grading-area
+
+javac -cp $CPATH grading-area/*.java
+
+if [[ $? -ne 0 ]]
+then
+  echo 'Files not compiling'
+  echo 'Score 0/4'
+  exit 1
+fi 
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
